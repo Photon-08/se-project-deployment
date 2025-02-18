389221ddfeaffb7f1453cde09151dc1c55732abd
INDRANIL BHATTACHARYYA <125260412+Photon-08@users.noreply.github.com>
2025-02-18 23:16:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from backend.config import LocalDevelopmentConfig
 from backend.model import db, User, Role
 from flask_security import Security, SQLAlchemyUserDatastore
-
+import os
 
 
 def createApp():
@@ -35,3 +35,8 @@ import backend.router
 #please uncomment this for dev
 #if (__name__ == '__main__'):
 #    app.run()
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))  # Get port from env or default
+    host = "0.0.0.0"  # Listen on all interfaces (important for Cloud Run)
+    app.run(debug=True, host=host, port=port)  # Run app with host and port
